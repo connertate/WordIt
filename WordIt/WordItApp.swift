907730da371607809f07189dcbe5bcb653fe26c9
Conner Tate <connertate8@gmail.com>
2022-05-14 15:24:24 -0400

    Initial Commit

---
+++ WordIt/WordItApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordItApp.swift
+//  WordIt
+//
+//  Created by Conner Tate on 5/14/22.
+//
+
+import SwiftUI
+
+@main
+struct WordItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
